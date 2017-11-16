e02c76b7635144ee887699b9b504ee1ec3b1a24c
Ryan Stalbow <ryan.stalbow@uktv.co.uk>
2017-11-16 14:27:20 +0000

    new url format

--- cas_provider/urls.py
+++ cas_provider/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url
+
+from cas_provider.views import login, logout, service_validate, proxy, validate, proxy_validate
 
 
-urlpatterns = patterns('cas_provider.views',
-    url(r'^login/merge/', 'login', {'merge': True, 'template_name': 'cas/merge.html'}),
-    url(r'^login/?$', 'login', name='cas_login'),
-    url(r'^socialauth-login/$', 'login', name='cas_socialauth_login'),
-    url(r'^validate/?$', 'validate', name='cas_validate'),
-    url(r'^proxy/?$', 'proxy', name='proxy'),
-    url(r'^serviceValidate/?$', 'service_validate', name='cas_service_validate'),
-    url(r'^proxyValidate/?$', 'proxy_validate', name='cas_proxy_validate'),
-    url(r'^logout/?$', 'logout', name='cas_logout'),
-)
+urlpatterns = [
+    url(r'^login/merge/', login, {'merge': True, 'template_name': 'cas/merge.html'}),
+    url(r'^login/?$', login, name='cas_login'),
+    url(r'^socialauth-login/$', login, name='cas_socialauth_login'),
+    url(r'^validate/?$', validate, name='cas_validate'),
+    url(r'^proxy/?$', proxy, name='proxy'),
+    url(r'^serviceValidate/?$', service_validate, name='cas_service_validate'),
+    url(r'^proxyValidate/?$', proxy_validate, name='cas_proxy_validate'),
+    url(r'^logout/?$', logout, name='cas_logout'),
+]
